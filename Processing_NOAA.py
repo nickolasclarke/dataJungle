40109c89f5b457e30e7ca19b02f571af869a89a8
Margaret McCall <margaretmccall@Margarets-MacBook-Air.local>
2019-12-11 18:20:14 -0800

    added script to format NOAA data for single-point model

--- Processing_NOAA.py
+++ Processing_NOAA.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
 # # <font color='yellow'>Exploring & processing NOAA data</font>
 # 
 # 
 # Sections:
 # * Loading and exploring data (NEED TO EXPLORE)
 # * Processing data to fit single-point and multi-point models
-
+# %% [markdown]
 # ### <font color='yellow'>Loading data (still need to do EDA!!)</font>
 
-# In[6]:
-
-
+# %%
 import json
 import csv
 import pandas as pd
@@ -30,16 +28,12 @@ import datetime
 pd.set_option('display.max_columns', 500)
 
 
-# In[7]:
-
-
+# %%
 path = r'/Users/margaretmccall/Downloads/final'
 all_files = glob.glob(path + "/*.csv")
 
 
-# In[8]:
-
-
+# %%
 dfs = []
 
 for file in all_files:
@@ -50,63 +44,49 @@ for file in all_files:
         bad_files.append(file)
 
 
-# In[9]:
-
-
+# %%
 df = pd.concat(dfs, ignore_index=True)
 
 
-# In[10]:
-
-
+# %%
 df.head()
 
 
-# In[11]:
-
-
+# %%
 #Picking the most relevant-seeming columns
 df = df[['Date_Time','Station_ID','wind_direction_set_1','wind_speed_set_1','pressure_set_1d','precip_accum_one_hour_set_1','air_temp_set_1']]
 
 
-# In[12]:
-
-
+# %%
 df = df.rename(columns={"Date_Time":"datetime"})
 
 
-# In[13]:
-
-
+# %%
 df.head()
 
 
-# In[14]:
-
-
+# %%
 df.to_csv("NOAA_Data_Complete.csv", index=False)
 
+# %% [markdown]
+# # <font color='yellow'>Processing NOAA data (run from here down to reformat data)</font>
 
-# # <font color='yellow'>Processing NOAA data</font>
+# %%
+df = pd.read_csv("NOAA_Data_Complete.csv")
 
+# %% [markdown]
 # ## <font color='yellow'>Getting station coordinates</font>
 
-# In[15]:
-
-
+# %%
 #Processing station coords
 coords = pd.read_csv("noaa_ca_stations.csv")
 
 
-# In[16]:
+# %%
+coords.head(1)
 
 
-coords.head()
-
-
-# In[17]:
-
-
+# %%
 import shapely.wkt
 lons, lats = [], []
 
@@ -116,58 +96,81 @@ for i in range(len(coords['geometry'])):
     lats.append(lat[0])
 
 
-# In[18]:
-
-
+# %%
 noaa_coords = coords[['stationIdentifier']]
 
 
-# In[19]:
+# %%
+noaa_coords.loc[:,'latitude'] = lats
+noaa_coords.loc[:,'longitude'] = lons
 
 
-noaa_coords['latitude'] = lats
-noaa_coords['longitude'] = lons
-
-
-# In[20]:
-
-
+# %%
 noaa_coords.to_csv("NOAA_Coordinates.csv", index=False)
 
 
-# ### <font color='yellow'>Shaping data for single-point model</font>
-
-# In[21]:
-
-
-#TBD
-
-
-# ### <font color='yellow'>Shaping data for multi-point model</font>
-
-# In[22]:
-
-
+# %%
 #Adding coords
 noaa_coords.rename(columns={'stationIdentifier':'Station_ID'}, inplace=True)
 
 
-# In[23]:
-
-
+# %%
 df_noaa = df
 df_noaa = df_noaa.merge(noaa_coords, on='Station_ID', how='left')
 
 
-# In[24]:
+# %%
+df_noaa = df_noaa.rename(columns={'Station_ID':'name'})
 
+# %% [markdown]
+# ### <font color='yellow'>Shaping data for single-point model</font>
 
+# %%
 df_noaa.head()
 
 
-# In[25]:
+# %%
+df_noaa = df_noaa.drop(columns=['latitude','longitude'])
 
 
+# %%
+data_types = df_noaa.columns.tolist()[2:7]
+
+DataFrameDict = {elem : pd.DataFrame for elem in data_types}
+
+
+# %%
+for key in DataFrameDict.keys():
+    DataFrameDict[key] = df_noaa[[key,'datetime','name']]
+
+
+# %%
+DataFrameDict['wind_speed_set_1'].head()
+
+
+# %%
+for key in DataFrameDict.keys():
+    DataFrameDict[key] = DataFrameDict[key].pivot_table(index='datetime',columns='name',values=key)
+
+
+# %%
+DataFrameDict['wind_speed_set_1'].head(1)
+#wow, this worked like a charm
+
+
+# %%
+#Outputting each data type into separate CSV
+for key in DataFrameDict.keys():
+    DataFrameDict[key].to_csv("NOAA"+key+".csv")
+
+# %% [markdown]
+# ### <font color='yellow'>Shaping data for multi-point model</font>
+
+# %%
+df_noaa.head()
+
+
+# %%
 #We want every dataframe's columns in the following order: datetime, lat, lon, name, measurement1, measurement2, ...
 cols = ['datetime',  'latitude', 'longitude', 'Station_ID',
  'wind_direction_set_1',
@@ -176,23 +179,17 @@ cols = ['datetime',  'latitude', 'longitude', 'Station_ID',
  'precip_accum_one_hour_set_1',
  'air_temp_set_1',
 ]
-df_noaa = df_noaa[cols].rename(columns={'Station_ID':'name'})
+df_noaa = df_noaa[cols]
 
 
-# In[26]:
-
-
+# %%
 df_noaa.head(1)
 
 
-# In[28]:
-
-
+# %%
 df_noaa.to_csv("NOAA_Data_MultiPointModel.csv", index=False)
 
 
-# In[ ]:
-
-
+# %%
 
 

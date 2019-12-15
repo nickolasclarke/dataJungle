cdbe00c06ef8a048f2fd6211f13055af4615e589
Jake McDermott <jake_mcdermott@berkeley.edu>
2019-12-14 16:34:37 -0800

    adding notebook

---
+++ Intro_pieces_final_notebook.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+## Input Data Description (5 points)
+Here you will provide an initial description of your data sets, including:
+get_ipython().set_next_input('1. The origins of your data.  Where did you get the data?  How were the data collected from the original sources');get_ipython().run_line_magic('pinfo', 'sources')
+2. The structure, granularity, scope, temporality and faithfulness (SGSTF) of your data.  To discuss these attributes you should load the data into one or more data frames (so you'll start building code cells for the first time).  At a minimum, use some basic methods (`.head`, `.loc`, and so on) to provide support for the descriptions you provide for SGSTF. 
+
+[Chapter 5](https://www.textbook.ds100.org/ch/05/eda_intro.html) of the DS100 textbook might be helpful for you in this section.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+## Data Cleaning (10 points)
+In this section you will walk through the data cleaning and merging process.  Explain how you make decisions to clean and merge the data.  Explain how you convince yourself that the data don't contain problems that will limit your ability to produce a meaningful analysis from them.  
+
+[Chapter 4](https://www.textbook.ds100.org/ch/04/cleaning_intro.html) of the DS100 textbook might be helpful to you in this section.  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+## Data Summary and Exploratory Data Analysis (10 points)
+
+In this section you should provide a tour through some of the basic trends and patterns in your data.  This includes providing initial plots to summarize the data, such as box plots, histograms, trends over time, scatter plots relating one variable or another.  
+
+[Chapter 6](https://www.textbook.ds100.org/ch/06/viz_intro.html) of the DS100 textbook might be helpful for providing ideas for visualizations that describe your data.  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
